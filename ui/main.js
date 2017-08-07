5590c26954c17e888562e256ae492107f2368a8a
Chaitanya2540 <30658577+Chaitanya2540@users.noreply.github.com>
2017-08-07 07:51:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,13 @@ var element = document.getElementById('main-text'
     
     // Move the image
     var img = document.getElementById('modi');
+    var marginleft = 0;
+    function moveRight(){
+        marginleft = marginleft + 10;
+        img.style.marginLeft = marginleft + 'px';
+    }
     
     img.onclick = function () {
-         img.style.marginLeft = '100px';
+         //img.style.marginLeft = '100px';
+         var interval = setInterval(moveRight, 100);
     };
