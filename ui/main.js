af3fc68c00f22719b276ddb657a16b9bd49b62b1
Chaitanya2540 <30658577+Chaitanya2540@users.noreply.github.com>
2017-08-07 07:37:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,8 @@ var element = document.getElementById('main-text'
     element.innerHTML = 'New Value';
     
     // Move the image
-    var img = document.getElementById('modi');
+    //var img = document.getElementById('modi');
+    var img = document.getElementByClass('medium');
     img.onclick = function () {
         img.style.marginleft = '100px';
     };
